de9b02651d4cb0e6c83627b7bd4c2407058e6418
Reo <liudev555@gmail.com>
2023-03-17 00:46:30 +0800

    fix: remove console log

--- src/helpers/render-document-file.js
+++ src/helpers/render-document-file.js
@@ -322,7 +322,6 @@ export async function convertVTreeToXML(docxDocumentInstance, vTree, xmlFragment
 }
 
 async function renderDocumentFile(docxDocumentInstance) {
-  console.log('render document: ', docxDocumentInstance);
   const vTree = convertHTML(docxDocumentInstance.htmlString?.replace(/&nbsp;/g, ' '));
 
   const xmlFragment = fragment({ namespaceAlias: { w: namespaces.w } });
